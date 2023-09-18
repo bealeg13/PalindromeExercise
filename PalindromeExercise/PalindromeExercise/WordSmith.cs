a6c872d1bb29faaefbd4fb632d7ca4f4ab35cf24
Gabriel Bealer <bealeg13@gmail.com>
2023-09-18 10:05:35 -0700

    finished exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string input)
+        {
+            //input = input.Replace(" ", "");
+
+            //int length = input.Length;
+            //for (int i = 0; i < length / 2; i++)
+            //{
+            //    if (input[i] != input[length - i - 1])
+            //    {
+            //        return false;
+            //    }
+            //}
+            //return true;
+
+            string reverseWord = new string(input.Reverse().ToArray());
+            return input == reverseWord;
+            
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,21 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("madam", true)]
+        [InlineData("hello", false)]
+        [InlineData("noon", true)]
+        [InlineData("power", false)]
+        
+        public void PalindromeTest(string input, bool expected)
         {
+            var instance = new WordSmith();
 
+            var actual = instance.IsAPalindrome(input);
+
+            Assert.Equal(expected, actual);
         }
+        
     }
 }
